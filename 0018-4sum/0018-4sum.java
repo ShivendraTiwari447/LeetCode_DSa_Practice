3a981375684176154a43f0b1a4197729522ba2ef
ShivendraTiwari447 <shivendratiwari447@gmail.com>
2026-08-17 22:07:03 +0530

    Time: 21 ms (41.72%), Space: 45.6 MB (88.9%) - LeetHub

---
+++ 0018-4sum/0018-4sum.java
@@ -0,0 +1,73 @@
+import java.util.*;
+
+class Solution {
+    public List<List<Integer>> fourSum(int[] nums, int target) {
+
+        int n = nums.length;
+
+        Arrays.sort(nums);
+
+        List<List<Integer>> ans = new ArrayList<>();
+
+        for (int i = 0; i < n - 3; i++) {
+
+            // Duplicate i skip
+            if (i > 0 && nums[i] == nums[i - 1]) {
+                continue;
+            }
+
+            for (int j = i + 1; j < n - 2; j++) {
+
+                // Duplicate j skip
+                if (j > i + 1 && nums[j] == nums[j - 1]) {
+                    continue;
+                }
+
+                int left = j + 1;
+                int right = n - 1;
+
+                while (left < right) {
+
+                    long sum = (long) nums[i]
+                             + nums[j]
+                             + nums[left]
+                             + nums[right];
+
+                    if (sum == target) {
+
+                        ans.add(Arrays.asList(
+                            nums[i],
+                            nums[j],
+                            nums[left],
+                            nums[right]
+                        ));
+
+                        // Skip duplicate left
+                        while (left < right &&
+                               nums[left] == nums[left + 1]) {
+                            left++;
+                        }
+
+                        // Skip duplicate right
+                        while (left < right &&
+                               nums[right] == nums[right - 1]) {
+                            right--;
+                        }
+
+                        left++;
+                        right--;
+
+                    }
+                    else if (sum < target) {
+                        left++;
+                    }
+                    else {
+                        right--;
+                    }
+                }
+            }
+        }
+
+        return ans;
+    }
+}
